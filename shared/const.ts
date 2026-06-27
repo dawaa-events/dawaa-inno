101f5694280c20abb6c096594babb5801f35f429
dawaa-events <dawaa.events@gmail.com>
2026-06-28 01:15:44 +0400

    Add files via upload

--- shared/const.ts
+++ shared/const.ts
@@ -1,1 +1,5 @@
 export const UNAUTHED_ERR_MSG = "Unauthorized";
+export const NOT_ADMIN_ERR_MSG = "Not admin";
+export const COOKIE_NAME = "dawaa_session";
+export const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
+export const AXIOS_TIMEOUT_MS = 30_000;
